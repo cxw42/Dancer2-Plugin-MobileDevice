c0d31463aa33ddb5ccc76a5d3b5a50ee64f39532
Yanick Champoux <yanick@cpan.org>
2019-12-08 13:48:05 -0500

    Copied in Dancer::Plugin::MobileDevice

--- lib/Dancer2/Plugin/MobileDevice.pm
+++ lib/Dancer2/Plugin/MobileDevice.pm
@@ -1,39 +1,123 @@
-package Dancer2::Plugin::MobileDevice;
-use 5.008001;
+package Dancer::Plugin::MobileDevice;
+#ABSTRACT: make a Dancer app mobile-aware
+
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+use Dancer ':syntax';
+use Dancer::Plugin;
 
+register 'is_mobile_device' => sub {
+    no warnings 'uninitialized';
+    return request->user_agent 
+        =~ /(?:iP(?:ad|od|hone)|Android|BlackBerry|Mobile|Palm)/ || 0 ;
+};
 
+hook before => sub {
+    # If we don't have a mobile layout declared, do nothing.
+    my $mobile_layout = plugin_setting->{mobile_layout}
+        and is_mobile_device()
+        or return;
+
+    var orig_layout => setting('layout');
+    setting layout => $mobile_layout;
+};
+
+hook after => sub {
+    setting layout => delete vars->{orig_layout}
+        if plugin_setting->{mobile_layout}
+        and is_mobile_device();
+};
+
+hook before_template => sub {
+    my $tokens = shift;
+    $tokens->{'is_mobile_device'} = is_mobile_device();
+};
+
+register_plugin;
 
 1;
 __END__
 
-=encoding utf-8
-
-=head1 NAME
-
-Dancer2::Plugin::MobileDevice - It's new $module
-
 =head1 SYNOPSIS
 
-    use Dancer2::Plugin::MobileDevice;
+    package MyWebApp;
+    use Dancer;
+    use Dancer::Plugin::MobileDevice;
+
+    get '/' => sub {
+        if (is_mobile_device) {
+            # do something for mobile
+        }
+        else {
+            # do something for regular agents
+        }
+    };
 
 =head1 DESCRIPTION
 
-Dancer2::Plugin::MobileDevice is ...
+A plugin for L<Dancer>-powered webapps to easily detect mobile clients and offer
+a simplified layout, and/or act in different ways.
 
-=head1 LICENSE
+The plugin offers a C<is_mobile_device> keyword, which returns true if the
+device is recognised as a mobile device.
 
-Copyright (C) Chris White.
+It can also automatically change the layout used to render views for mobile
+clients.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=head1 Custom layout for mobile devices
 
-=head1 AUTHOR
+This plugin can use a custom layout for recognised mobile devices, allowing you
+to present a simplified page template for mobile devices.  To enable this, use
+the C<mobile_layout> setting for this plugin - for instance, add the following
+to your config file:
 
-Chris White E<lt>cxwembedded@gmail.comE<gt>
+  plugins:
+    MobileDevice:
+      mobile_layout: 'mobile'
+
+This means that, when C<template> is called to render a view, if the client is
+recognised as a mobile device, the layout named C<mobile> will be used, rather
+than whatever the current C<layout> setting is.
+
+You can of course still override this layout by supplying a layout option to the
+C<template> call in the usual way (see the L<Dancer> documentation for how to do
+this).
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+L<http://github.com/PerlDancer/Dancer-Plugin-MobileDevice/issues>
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::MobileDevice
+
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer-Plugin-MobileDevice>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer-Plugin-MobileDevice>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer-Plugin-MobileDevice/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+This plugin was initially written for an article of the Dancer advent
+calendar 2010.
 
 =cut
-
